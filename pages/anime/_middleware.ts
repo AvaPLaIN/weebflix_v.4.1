c7a38355701cf086adff2a0610df0eb43f6b505d
Kevin Voß <kevin.voss@micromerce.com>
2022-04-17 19:03:09 +0200

    adjust redirect url

--- pages/anime/_middleware.ts
+++ pages/anime/_middleware.ts
@@ -6,13 +6,14 @@ import { NextResponse } from "next/server";
 const secret = process.env.NEXTAUTH_SECRET;
 
 async function middleware(req: any) {
+  const redirectAuthUrl = process.env.REDIRECT_AUTH_URL || "/api/auth/signin";
+
   const session = await getToken({
     req,
     secret,
   });
 
-  if (!session)
-    return NextResponse.redirect("http://localhost:3000/api/auth/signin");
+  if (!session) return NextResponse.redirect(redirectAuthUrl);
 }
 
 export default middleware;
