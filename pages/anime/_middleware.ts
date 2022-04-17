c0058397b65e8d98c0d680395736c617088496e8
Kevin Voß <kevin.voss@micromerce.com>
2022-04-17 20:14:00 +0200

    remove unused code in middleware

--- pages/anime/_middleware.ts
+++ pages/anime/_middleware.ts
@@ -1,19 +1,1 @@
 export { default } from "next-auth/middleware";
-
-// import { getToken } from "next-auth/jwt";
-// import { NextResponse } from "next/server";
-
-// const secret = process.env.NEXTAUTH_SECRET;
-
-// async function middleware(req: any) {
-//   const redirectAuthUrl = process.env.REDIRECT_AUTH_URL || "/api/auth/signin";
-
-//   const session = await getToken({
-//     req,
-//     secret,
-//   });
-
-//   if (!session) return NextResponse.redirect(redirectAuthUrl);
-// }
-
-// export default middleware;
