96417613d2cbef0e94e1331cc996164907edd338
Kevin Voß <kevin.voss@micromerce.com>
2022-04-18 14:41:55 +0200

    dashboard pages

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import Header from "../../components/layout/Head";
 import DashboardSidebar from "../../components/layout/dashboard-sidebar";
 import styles from "../../styles/dashboard.module.css";
+import { useSession } from "next-auth/react";
 
 const Dashboard = () => {
+  const { data } = useSession();
+
   return (
     <div className={styles.layout}>
       <Header title="Weebflix - Dashboard" />
       <DashboardSidebar />
-      <h1>Dashboard</h1>
+      <h1>
+        Welcome <span>{data?.user?.name}</span> to Weebflix Dashboard Homepage
+      </h1>
     </div>
   );
 };
---
+++ pages/dashboard/planing/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "../../../components/layout/Head";
+import DashboardSidebar from "../../../components/layout/dashboard-sidebar";
+import styles from "../../../styles/dashboard.module.css";
+import { useSession } from "next-auth/react";
+
+const PlaningPage = () => {
+  const { data } = useSession();
+
+  return (
+    <div className={styles.layout}>
+      <Header title="Weebflix - Dashboard" />
+      <DashboardSidebar />
+      <h1>
+        Welcome <span>{data?.user?.name}</span> to Weebflix Dashboard Planing
+      </h1>
+    </div>
+  );
+};
+
+export default PlaningPage;
