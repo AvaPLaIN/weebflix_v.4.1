7c0eb0e26f3119bc3edbc1e59d590377b4210181
Kevin Voß <kevin.voss@micromerce.com>
2022-04-18 15:41:13 +0200

    implement create form for dashboard

--- components/layout/dashboard-sidebar/DashboardSidebar.tsx
+++ components/layout/dashboard-sidebar/DashboardSidebar.tsx
@@ -3,44 +3,53 @@ import { SidebarContainer } from "./DashboardSidebar.styles";
 import { BiArrowToLeft, BiArrowToRight, BiPlus } from "react-icons/bi";
 import sidebarConfig from "./data/sidebar-config";
 import NavItem from "./components/nav-item";
+import Form from "../../modules/pages/dashboard/form";
 
 const DashboardSidebar = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isCreateFormOpen, setIsCreateFormOpen] = useState(false);
 
   const handleSetSidebarIsOpen = () => {
     setIsSidebarOpen((isSidebarOpen) => !isSidebarOpen);
   };
 
+  const handleSetCreateFormOpen = () => {
+    setIsCreateFormOpen((isCreateFormOpen) => !isCreateFormOpen);
+  };
+
   const sidebarClassnames = isSidebarOpen ? "open" : "closed";
 
   return (
-    <SidebarContainer className={sidebarClassnames}>
-      <div className="sidebar-header-container">
-        <div className="header">
-          <h1>Dashboard</h1>
-          <div className="toggle" onClick={handleSetSidebarIsOpen}>
-            {isSidebarOpen ? (
-              <BiArrowToLeft className="toggle-sidebar-button" />
-            ) : (
-              <BiArrowToRight className="toggle-sidebar-button" />
-            )}
+    <>
+      <SidebarContainer className={sidebarClassnames}>
+        <div className="sidebar-header-container">
+          <div className="header">
+            <h1>Dashboard</h1>
+            <div className="toggle" onClick={handleSetSidebarIsOpen}>
+              {isSidebarOpen ? (
+                <BiArrowToLeft className="toggle-sidebar-button" />
+              ) : (
+                <BiArrowToRight className="toggle-sidebar-button" />
+              )}
+            </div>
+          </div>
+          <div className="create-container">
+            <div className="create-wrapper" onClick={handleSetCreateFormOpen}>
+              <BiPlus className="create-icon" />
+              <p>Create</p>
+            </div>
           </div>
         </div>
-        <div className="create-container">
-          <div className="create-wrapper">
-            <BiPlus className="create-icon" />
-            <p>Create</p>
-          </div>
-        </div>
-      </div>
-      <nav>
-        <ul className="nav-list">
-          {sidebarConfig.map((item) => (
-            <NavItem key={item.name} item={item} />
-          ))}
-        </ul>
-      </nav>
-    </SidebarContainer>
+        <nav>
+          <ul className="nav-list">
+            {sidebarConfig.map((item) => (
+              <NavItem key={item.name} item={item} />
+            ))}
+          </ul>
+        </nav>
+      </SidebarContainer>
+      {isCreateFormOpen && <Form />}
+    </>
   );
 };
 
---
+++ components/modules/pages/dashboard/form/Form.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <div>
+      <div className="header">
+        <h1>Create Form</h1>
+        <div className="close">Close</div>
+      </div>
+      <div className="form-container">
+        <form>Form</form>
+        <div className="controls">Controls</div>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
